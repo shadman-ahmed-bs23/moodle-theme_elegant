280c46ac271932e7b2ebf08c9888f7d340818959
Shadman <shadman.chowdhury@brainstation23.com>
2021-08-17 17:51:17 +0600

    Add initial skeleton for theme

---
+++ lang/en/theme_elegant.php
@@ -0,0 +1,25 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+// This line protects the file from being accessed by a URL directly.
+defined('MOODLE_INTERNAL') || die();
+
+// A description shown in the admin theme selector.
+$string['choosereadme'] = 'Theme elegant is a child theme of Boost. It adds the ability to upload background photos.';
+// The name of our plugin.
+$string['pluginname'] = 'Elegant Theme';
+// We need to include a lang string for each block region.
+$string['region-side-pre'] = 'Right';
